807449a13a7ee5288054d6e0d9caa4d4614ab763
William Crane <wcran7@gmail.com>
2025-08-06 19:55:22 +1000

    chore(appwrite): setup Appwrite client and connect to database

---
+++ ubereats-clone/lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  platform: "com.company.ubereats-clone",
+  databaseId: "689324fd000686f0d6a9",
+  userCollectionId: "689325a400186af8c08e",
+};
