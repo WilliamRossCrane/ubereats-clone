4776b83ba33b27b790de7956a6b76ebfe4437d07
William Crane <wcran7@gmail.com>
2025-08-18 20:08:35 +1000

    feat(seed): add database seeding script and update package.json

--- ubereats-clone/app/(tabs)/search.tsx
+++ ubereats-clone/app/(tabs)/search.tsx
@@ -1,14 +1,24 @@
-import { Text } from "react-native";
+import { Text, Button } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import seed from "@/lib/seed";
 
-const search = () => {
+const Search = () => {
+  const handleSeed = async () => {
+    try {
+      await seed();
+      console.log("Seeding successful");
+    } catch (error) {
+      console.log("Failed to seed:", error);
+    }
+  };
+
   return (
     <SafeAreaView>
       <Text>Search</Text>
+      <Button title="Seed" onPress={handleSeed} />
     </SafeAreaView>
   );
 };
 
-export default search;
+export default Search;
